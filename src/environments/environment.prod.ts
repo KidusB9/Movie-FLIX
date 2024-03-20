70735621352f103b54bcc10e60978b4826d9ae70
Kidus Berhanu <107410165+Kidus-berhanu@users.noreply.github.com>
2024-03-19 21:34:20 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,4 +3,5 @@ export const environment = {
   theMovieDBApi: '937dc42f1dd9b61d43dad5138fc43841',
   apiUrl: 'http://localhost:3000/api',
   openAIKey: '',
+  backendUrl: 'http://localhost:3000'
 };
